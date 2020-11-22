557570be238121bfa027bbbd397d54ce92eb8d78
taeha <teahe>
2020-11-22 17:54:57 +0900

    final

--- Phase4/src/service/MovieService.java
+++ Phase4/src/service/MovieService.java
@@ -14,14 +14,15 @@ import entity.Account;
 import entity.Rating;
 
 public class MovieService {
-//	private static String url = "jdbc:postgresql://localhost/testdb";
-//	private static String uid = "testdb";
-//	private static String pwd = "testdb";
-//	private static String driver = "org.postgresql.Driver";
-	private static String url = "jdbc:postgresql://localhost/knumovie";
-	private static String uid = "postgres";
-	private static String pwd = "comp322";
+	private static String url = "jdbc:postgresql://localhost/testdb";
+	private static String uid = "testdb";
+	private static String pwd = "testdb";
 	private static String driver = "org.postgresql.Driver";
+//	private static String url = "jdbc:postgresql://localhost/knumovie";
+//	private static String uid = "postgres";
+//	private static String pwd = "comp322";
+//	private static String driver = "org.postgresql.Driver";s
+			
 	
 	public static void show_all_movies() throws ClassNotFoundException, SQLException {
 		
@@ -32,10 +33,11 @@ public class MovieService {
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(sql);	
 		
-		System.out.println("2A. ALL MOVIEs ");
+		System.out.println("2A. ALL MOVIEs are");
 		
 		while(rs.next()) {
-			System.out.println(rs.getInt(1) +"    |    "+rs.getString(2));
+			System.out.printf("%3d", rs.getInt(1));
+			System.out.println(" " + rs.getString(2));
 		}
 	}
 	
@@ -50,8 +52,6 @@ public class MovieService {
 		Connection con = DriverManager.getConnection(url, uid, pwd);
 	
 		PreparedStatement st = con.prepareStatement(sql);
-		st = con.prepareStatement(sql);
-		
 	
 		st.setString(1, movie_title);
 		st.setString(2, loginuser);
@@ -61,7 +61,8 @@ public class MovieService {
 		System.out.println("2B. Result is ");
 		
 		while(rs.next()) {
-			System.out.println(rs.getInt(1) +"    |    "+rs.getString(2));
+			System.out.printf("%3d", rs.getInt(1));
+			System.out.println(" " + rs.getString(2));
 		}
 	}
 	
@@ -91,7 +92,9 @@ public class MovieService {
 		
 		while(rs.next()) {
 			
-			System.out.println(rs.getInt(1) +"    |    "+rs.getString(2));
+
+			System.out.printf("%3d", rs.getInt(1));
+			System.out.println(" " + rs.getString(2));
 			
 	
 			movie_list.add(rs.getString(2));
@@ -139,4 +142,63 @@ public class MovieService {
 		if (result == 1)
 			System.out.println("Rating created successfully");
 	}
+	
+	public static void movie_info(String movie_title) throws ClassNotFoundException, SQLException {
+		
+		String sql = "select * from movie natural join movie_genre where movie_title = ? ";
+	
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+	
+		PreparedStatement st = con.prepareStatement(sql);
+	
+		st.setString(1, movie_title);
+		
+		ResultSet rs = st.executeQuery();	
+		
+		System.out.println("2D. This movie info ");
+		
+		rs.next();
+		
+		System.out.println("    제목 : " + rs.getString(2));
+		System.out.println("    타입 : " + rs.getString(3));
+		if(rs.getString(3).equals("Series")) System.out.println("    소속 시리즈 : " + rs.getString(4));
+		if(rs.getString(3).equals("Series")) System.out.println("    시리즈 번호 : " + rs.getString(5));
+		if(rs.getBoolean(6)) System.out.println ("    성인등급영화");
+		else System.out.println ("    전체관람가");
+		System.out.println("    감독 : " + rs.getString(7) + " " + rs.getString(8));
+		System.out.println("    상영시작일 : " + rs.getString(9));
+		System.out.println("    상영시간 : " + rs.getString(10) + "분");
+
+		sql = "select genre_name from movie natural join movie_genre where movie_title = ?";
+		
+		st = con.prepareStatement(sql);
+		
+		st.setString(1, movie_title);
+		
+		rs = st.executeQuery();
+		
+		rs.next();
+		System.out.println("    장르 : " + rs.getString(1));
+		
+		sql = "select concat(cast_first_name, ' ', cast_last_name), concat(real_first_name, ' ', real_last_name) from (movie natural join starred_by) natural join actor where movie_title = ? ";
+		
+		st = con.prepareStatement(sql);
+		
+		st.setString(1, movie_title);
+		
+		rs = st.executeQuery();
+		
+		System.out.print("    출연진정보 : ");
+		
+		while(rs.next()) {
+		
+		System.out.print(rs.getString(1));
+		System.out.print("(" + rs.getString(2) + ") | ");
+		}
+		
+		System.out.println();
+	}
+	
+	
 }
