56c7f78faceb3449fc46876edccdbbc6336cb7f0
JongbinWoo <wjb1218@gmail.com>
2020-11-22 16:17:16 +0900

    push2

--- Phase4/src/service/MovieService.java
+++ Phase4/src/service/MovieService.java
@@ -10,19 +10,22 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MovieService {
-	private static String url = "jdbc:postgresql://localhost/testdb";
-	private static String uid = "testdb";
-	private static String pwd = "testdb";
-	private static String driver = "org.postgresql.Driver";
-//	private static String url = "jdbc:postgresql://localhost/knumovie";
-//	private static String uid = "postgres";
-//	private static String pwd = "comp322";
+//	private static String url = "jdbc:postgresql://localhost/testdb";
+//	private static String uid = "testdb";
+//	private static String pwd = "testdb";
 //	private static String driver = "org.postgresql.Driver";
+	private static String url = "jdbc:postgresql://localhost/knumovie";
+	private static String uid = "postgres";
+	private static String pwd = "comp322";
+	private static String driver = "org.postgresql.Driver";
 			
 	
-	public static String search_movie(String movie_title) throws ClassNotFoundException, SQLException {
+	public static void search_movie(String loginuser, String movie_title) throws ClassNotFoundException, SQLException {
 	
-		String sql = "SELECT * FROM (MOVIE NATURAL JOIN ACTOR) WHERE Movie_title = ?";
+		String sql = ""
+				+ "SELECT Movie_id, Movie_title From movie where movie_id in (SELECT Movie_id FROM Movie WHERE Movie_title = ?"
+				+ "EXCEPT SELECT R.Movie_id FROM Rating as R WHERE (R.Account_id  IN " + 
+				"		(SELECT A.Account_id FROM Account AS A WHERE A.User_id = ?)))";
 	
 		Class.forName(driver);
 		Connection con = DriverManager.getConnection(url, uid, pwd);
@@ -31,26 +34,31 @@ public class MovieService {
 		st = con.prepareStatement(sql);
 	
 		st.setString(1, movie_title);
-		ResultSet rs = st.executeQuery();
-	
-		rs.next();
+		st.setString(2, loginuser);
 		
-		System.out.print("2A. Result is ");
-		for(int i = 1; i<11 ; i++) System.out.print(rs.getString(i)+" / ");
-		System.out.println("");
+		ResultSet rs = st.executeQuery();	
 		
-		String movie = rs.getString(1);
-		return movie;
+		System.out.println("2B. Result is ");
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt(1) +"    |    "+rs.getString(2));
+		}
 	}
 	
-	public static ArrayList<String> srch_movie(String type, String genre_name, String version_id) throws ClassNotFoundException, SQLException {
+	public static ArrayList<String> srch_movie(String loginuser, String type, String genre_name, String version_id) throws ClassNotFoundException, SQLException {
 	
-		String sql = "SELECT * FROM MOVIE WHERE movie_id IN (SELECT MOVIE.movie_id FROM MOVIE_GENRE, MOVIE FULL OUTER JOIN VERSION ON MOVIE.movie_id=VERSION.movie_id WHERE MOVIE.movie_id = MOVIE_GENRE.movie_id";
+		String sql = "SELECT Movie_id, Start_year FROM MOVIE WHERE movie_id IN (SELECT MOVIE.movie_id FROM MOVIE_GENRE, MOVIE FULL OUTER JOIN VERSION ON MOVIE.movie_id=VERSION.movie_id WHERE MOVIE.movie_id = MOVIE_GENRE.movie_id";
 		if(!(type.equals(""))) sql += " and Type = '" + type + "'";		
 		if(!(genre_name.equals(""))) sql += " and Genre_name = '" + genre_name + "'";
 		if(!(version_id.equals(""))) sql += " and Version_id = " + version_id;		
 		sql+= ")";
-
+		
+		sql += "MINUS  "
+				+ "Movie_id FROM Rating as R WHERE R.Account_id IN (SELECT A.Account_id FROM Account WHERE A.User_id = "
+				+ loginuser
+				+ ")";
+		
+		
 		Class.forName(driver);
 		Connection con = DriverManager.getConnection(url, uid, pwd);
 		Statement st = con.createStatement();
